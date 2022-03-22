36da6938d0eff094efb35b491d10000077c28fd4
enrico giorgi <enrico.giorgi8@studio.unibo.it>
2022-03-22 11:01:25 +0100

    bel lavoro P

--- projectwork8/Studente.php
+++ projectwork8/Studente.php
@@ -1,15 +1,11 @@
 
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
 
 /**
  * Description of Studente
  *
- * @author elpen
+ * @author erProf
  */
 class Studente extends Persona {
     public $ind_studio = "";
